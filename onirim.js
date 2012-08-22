396383faee1af563e17048575fb3eaa6703a7f6c
Rusty Bailey <rustylbailey@gmail.com>
2012-08-21 21:39:09 -0400

    Added functions to draw initial hand of 5
    Arrays for different stacks, initialDraw() deals to hand or limbo
    depending on cardType, drawFirstFive fills the hand with five beginning
    cards

--- onirim.js
+++ onirim.js
@@ -1,8 +1,7 @@
 /*
-Make a draw function.
-Add a card to the hand.
-3 more arrays -  Hand, Limbo, PlayArea(or something)
-Experiment with adding cards to limbo, playarea, etc., so that cards are never repeated.
+Notes:
+Need to deal with limbo from initialDraw
+Continue game after drawing first hand
 */
 
 
@@ -17,22 +16,22 @@ function Card(cardName, cardType, amount){
 var obSun = new Card("Observatory", "Sun", 9);
 var obMoon = new Card("Observatory", "Moon", 4);
 var obKey = new Card("Observatory", "Key", 3);
-var	obDoor = new Card("Observatory", "Door", 2);
+var obDoor = new Card("Observatory", "Door", 2);
 var aqSun = new Card("Aquarium", "Sun", 8);
 var aqMoon = new Card("Aquarium", "Moon", 4);
 var aqKey = new Card("Aquarium", "Key", 3);
-var	aqDoor = new Card("Aquarium", "Door", 2);
+var aqDoor = new Card("Aquarium", "Door", 2);
 var gaSun = new Card("Garden", "Sun", 7);
 var gaMoon = new Card("Garden", "Moon", 4);
 var gaKey = new Card("Garden", "Key", 3);
-var	gaDoor = new Card("Garden", "Door", 2);
+var gaDoor = new Card("Garden", "Door", 2);
 var liSun = new Card("Library", "Sun", 6);
 var liMoon = new Card("Library", "Moon", 4);
 var liKey = new Card("Library", "Key", 3);
-var	liDoor = new Card("Library", "Door", 2);
+var liDoor = new Card("Library", "Door", 2);
 var drMare = new Card("Dream", "Nightmare", 10);
 
-//somehow need to automatically make this an array of the cards
+//Making an array of unique cards
 var cards = [obSun, obMoon, obKey, obDoor, 
 			aqSun, aqMoon, aqKey, aqDoor,
 			gaSun, gaMoon, gaKey, gaDoor,
@@ -47,6 +46,42 @@ for(var i = 0; i < cards.length; i++){
 	}
 }
 
-//Print deck
-console.log(deck);
+//Draw a card from the deck
+var draw = function(){
+	var card = deck[Math.floor(Math.random()*deck.length)];
+	return card;
+};
+
+//Declaring other stacks
+var hand = [];
+var limbo = [];
+var playArea = [];
+var doorsGained = [];
+
+
+//***initialDraw does not move limbo cards back to deck yet*** Maybe it should be a different function?
+var initialDraw = function(){
+	var newCard = draw();
+	if(newCard.cardType === "Nightmare" || newCard.cardType === "Door"){
+		deck.pop(newCard);
+		limbo.push(newCard);
+	} else {
+		deck.pop(newCard);
+		hand.push(newCard);
+	}
+};
+
+//drawFirstFive draws until the first 5 cards in hand are not Doors or Nightmares
+var drawFirstFive = function(){
+	initialDraw();
+	if(hand.length != 5){
+		drawFirstFive();
+	}
+}
+
+//Tests
+
+drawFirstFive();
+console.log(hand);
+console.log(limbo);
 console.log(deck.length);
